d3838c43c29c3cd38168a01e6084545c30763892
Alberto Beloni <>
2019-01-04 16:26:02 -0200

    Fix command running as root

--- modules/bash/configure.sh
+++ modules/bash/configure.sh
@@ -1,5 +1,8 @@
 pre()
 {
+	command echo "/usr/local/bin/bash" | command sudo tee -a "/etc/shells"
+	command chsh -s "/usr/local/bin/bash"
+
 	if command test ! -r "/etc/bashrc.old" && command test -r "/etc/bashrc"
 	then
 		command sudo cp "/etc/bashrc" "/etc/bashrc.old"
--- modules/bash/install.sh
+++ modules/bash/install.sh
@@ -1,5 +1,2 @@
 command brew install bash
 command brew install bash-completion
-
-command echo "/usr/local/bin/bash" | command sudo tee -a "/etc/shells"
-command sudo chsh -s "/usr/local/bin/bash"

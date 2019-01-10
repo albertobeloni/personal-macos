fb0692eda8fb754d148fcc9e97f84b9f6f0d9cff
Alberto Beloni <>
2019-01-10 16:51:28 -0200

    Fix Git configuration using wrong file

--- modules/git/configure.sh
+++ modules/git/configure.sh
@@ -2,14 +2,14 @@ post()
 {
 	if command hash "nvim" > "/dev/null" 2>&1
 	then
-		command git config --global core.pager "nvim -R"
-		command git config --global color.pager no
+		command git config --file "${HOME}/.local/application/git/config" core.pager "nvim -R"
+		command git config --file "${HOME}/.local/application/git/config" color.pager no
 	fi
 
 	command local name
 
 	command read -p "Name: " name
-	command git config --global user.name "${name}"
+	command git config --file "${HOME}/.local/application/git/config" user.name "${name}"
 
 	command unset -v name
 }
